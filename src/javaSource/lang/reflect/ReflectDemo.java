3202333ea04e09f218976d07f8c620278d510f41
EdmanWang <2569320889@qq.com>
2019-07-19 16:51:29 +0800

    add java reflect source code

--- src/javaSource/lang/reflect/ReflectDemo.java
+++ src/javaSource/lang/reflect/ReflectDemo.java
@@ -1,4 +1,26 @@
 package javaSource.lang.reflect;
 
+import javaSource.lang.reflect.util.CustomerClass;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+// 反射测试demo
 public class ReflectDemo {
+
+    @Test // 测试 isAccessible 函数
+    public void testIsAccessibleField() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        CustomerClass customerClassClass = CustomerClass.class.newInstance();
+
+        Field privateField = CustomerClass.class.getDeclaredField("privateField");
+        Field annotationField = CustomerClass.class.getDeclaredField("annotationField");
+
+        System.out.println(privateField.get("wgx"));
+        boolean flag1 = privateField.isAccessible();
+        System.out.println(flag1);
+        boolean flag = annotationField.isAccessible();
+        System.out.println(flag);
+
+    }
+
 }
--- src/javaSource/lang/reflect/util/CustomerClass.java
+++ src/javaSource/lang/reflect/util/CustomerClass.java
@@ -1,4 +1,35 @@
 package javaSource.lang.reflect.util;
 
+@CustomAnnotation(name = "wgx", value = "qaz")
 public class CustomerClass {
+    public String publicFiled;
+
+    private String privateField;
+
+    @CustomAnnotation(name = "annotationField")
+    private String annotationField;
+
+    public String getPublicFiled() {
+        return publicFiled;
+    }
+
+    public void setPublicFiled(String publicFiled) {
+        this.publicFiled = publicFiled;
+    }
+
+    public String getPrivateField() {
+        return privateField;
+    }
+
+    public void setPrivateField(String privateField) {
+        this.privateField = privateField;
+    }
+
+    public String getAnnotationField() {
+        return annotationField;
+    }
+
+    public void setAnnotationField(String annotationField) {
+        this.annotationField = annotationField;
+    }
 }
