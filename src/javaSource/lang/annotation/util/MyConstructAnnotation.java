5147b8128f8deb872cfcd7deaa5355d68bc83fc1
EdmanWang <2569320889@qq.com>
2019-07-19 16:48:36 +0800

    ”uupdate code

--- src/javaSource/lang/annotation/util/MyConstructAnnotation.java
+++ src/javaSource/lang/annotation/util/MyConstructAnnotation.java
@@ -5,9 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)   //  设置注解的类型 -- 类型注解   --- 作用于类或者接口
+@Target(ElementType.CONSTRUCTOR)   //  设置注解的类型 -- 类型注解   --- 构造方法
 @Retention(RetentionPolicy.RUNTIME)  // 设置注解的生命周期
-public @interface MyClassAnnotation {
+public @interface MyConstructAnnotation {
     public int id() default -1;
 
     public String msg() default "Hi";
