887a2b86cda053f6205ace5590a4f31f13bb4468
EdmanWang <2569320889@qq.com>
2019-08-09 10:31:22 +0800

    add java securityManager source code

--- src/javaSource/securityManager/SecurityManagerDemo.java
+++ src/javaSource/securityManager/SecurityManagerDemo.java
@@ -1,4 +1,24 @@
 package javaSource.securityManager;
 
+import javaSource.securityManager.util.CustomerSecurityManager;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class SecurityManagerDemo {
+
+    @Test
+    public void test() throws IOException {
+        SecurityManager securityManager = new CustomerSecurityManager();
+        System.setSecurityManager(securityManager);
+
+        FileInputStream fis = new FileInputStream(new File("src/javaSource/securityManager/util/wgx.txt"));
+
+        int read = fis.read();
+        System.out.println(read);
+    }
 }
--- src/javaSource/securityManager/util/CustomerSecurityManager.java
+++ src/javaSource/securityManager/util/CustomerSecurityManager.java
@@ -1,4 +1,22 @@
 package javaSource.securityManager.util;
 
-public class CustomerSecurityManager {
+// 自定义的安全管理器
+public class CustomerSecurityManager extends SecurityManager {
+
+    @Override
+    public void checkRead(String file) {
+//        if (file != null && file.contains("wgx.txt")) {
+//            throw new RuntimeException("您还没有读的权限");
+//        }
+        super.checkRead(file);
+    }
+
+    @Override
+    public void checkWrite(String file) {
+        super.checkWrite(file);
+        if (file != null && file.contains("wgx.txt")) {
+            throw new RuntimeException("您还没有写的权限");
+        }
+        super.checkRead(file);
+    }
 }
